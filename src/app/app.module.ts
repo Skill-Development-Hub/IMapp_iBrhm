d656e7cc5da43deb48280d84b99b82b280e2a1e0
TAHU <tahu@example.com>
2024-10-01 15:47:20 +0530

    Sign-In page made

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,46 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SigninComponent } from './signin/signin.component';
+import { RegisterComponent } from './register/register.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+
+import { CommonModule } from '@angular/common';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatCardModule } from '@angular/material/card';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SigninComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CommonModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+
+    ReactiveFormsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatSnackBarModule,
+    MatCardModule,
   ],
-  providers: [],
+  providers: [
+    provideAnimationsAsync()
+  ],
+  exports: [SigninComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/signin/signin.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { MatSnackBar} from '@angular/material/snack-bar'
+
+
+@Component({
+  selector: 'app-signin',
+  templateUrl: './signin.component.html',
+  styleUrl: './signin.component.css',
+})
+export class SigninComponent {
+  signInForm: FormGroup;
+
+  constructor(private fb: FormBuilder, private snackBar: MatSnackBar){
+    this.signInForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+    });
+  }
+
+  onSubmit(){
+    if(this.signInForm.valid){
+      this.snackBar.open('Sign in successful!', 'Close', {
+        duration: 2000, });
+    }
+    else {
+      this.snackBar.open('Please fill in all fields correctly.', 'Close', {
+        duration: 2000,
+      });
+    }
+  }
+
+}

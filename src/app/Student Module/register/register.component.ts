0b5e3c1d6486f0e24d37703388461e34bcf75c42
Sd_hub iBrhm <ibbuhussain@gmail.com>
2024-10-07 13:50:29 +0530

    Stepper added

---
+++ src/app/Student Module/register/register.component.ts
@@ -0,0 +1,36 @@
+import { Component, inject } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.css'
+})
+export class RegisterComponent {
+  studentName: string = ' ';
+
+  constructor(private router: Router){}
+
+
+  navigateToWelcome(){
+    this.router.navigate(['/welcome-stdnt',this.studentName]);
+  }
+
+  private _formBuilder = inject(FormBuilder);
+
+  firstFormGroup = this._formBuilder.group({
+    firstCtrl: ['', Validators.required],
+  });
+  secondFormGroup = this._formBuilder.group({
+    secondCtrl: ['', Validators.required],
+  });
+  isLinear = true;
+
+}
+
+
+
+
